c95e477925ff623fbdc83229c4cb5bf99869e0ad
Krittika-Tiwari <krittikatiwari122004@outlook.com>
2024-07-12 00:37:35 +0530

    project set up with authentication and authorization

---
+++ app/test/subroute/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      i love you honey❤️❤️❤️❤️❤️❤️❤️
+    </div>
+  )
+}
+
+export default page
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://flying-coyote-8.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
